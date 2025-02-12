7ab0723ef139067d27bd55328f71f40a6f64d824
bastienggg <bastienguitard8@gmail.com>
2025-02-12 16:19:24 +0100

    Refactor LegoController and LegoService to support multiple Lego collections; update Twig templates for improved rendering

--- src/Controller/LegoController.php
+++ src/Controller/LegoController.php
@@ -14,13 +14,34 @@ class LegoController extends AbstractController
     #[Route('/', name: 'home')]
     public function home(LegoService $legoService): Response
     {
-        $lego = $legoService->getLego();
-        return $this->render('lego.html.twig', ['lego' => $lego]);
+        $response = new Response();
+        $legos = $legoService->getLegos();
+        foreach ($legos as $lego) {
+            $response->setContent($response->getContent() . $this->renderView('lego.html.twig', ['lego' => $lego]));
+        }
+        return $response;
     }
 
-    #[Route('/bastieng', name: 'bastieng')]
+    #[Route('/{collection}', name: 'filter_by_collection', requirements: ['collection' => 'creator|star_wars|creator_expert'])]
+    public function filter(string $collection, LegoService $legoService): Response
+    {
+        $response = new Response();
+        if ($collection == 'star_wars') {
+            $collection = 'Star Wars';
+        } else if ($collection == 'creator_expert') {
+            $collection = 'creator expert';
+        }
+        $legos = $legoService->getLegosByCollection($collection);
+        foreach ($legos as $lego) {
+            $response->setContent($response->getContent() . $this->renderView('lego.html.twig', ['lego' => $lego]));
+        }
+        return $response;
+    }
+
+    #[Route('/me', name: 'me')]
     public function me()
     {
-        die("bastieng");
+        die("BradSavary");
     }
 }
+?>
--- src/Service/LegoService.php
+++ src/Service/LegoService.php
@@ -4,13 +4,59 @@
 namespace App\Service;
 
 use App\Entity\Lego;
+use \PDO;
 
 class LegoService
 {
-    public function getLego(): Lego
+    private $pdo;
+
+    public function __construct()
     {
-    $lego = new Lego(1, 'Volkswagen Beetle', 'Creator Expert');
-    return $lego;
+        $dsn = 'mysql:host=tp-symfony-mysql;dbname=lego_store';
+        $username = 'root';
+        $password = 'root';
+        $this->pdo = new PDO($dsn, $username, $password);
     }
+
+        public function getLegos(): array
+        {
+            $stmt = $this->pdo->prepare('SELECT * FROM lego');
+            $stmt->execute();
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $legos = [];
+            foreach ($rows as $row) {
+                $lego = new Lego($row['id'], $row['name'], $row['collection']);
+                $lego->setDescription($row['description']);
+                $lego->setPrice($row['price']);
+                $lego->setPieces($row['pieces']);
+                $lego->setBoxImage($row['imagebox']);
+                $lego->setBackgroundImage($row['imagebg']);
+                $legos[] = $lego;
+            }
+
+            return $legos;
+        }
+        public function getLegosByCollection(string $collection): array
+        {
+            $stmt = $this->pdo->prepare('SELECT * FROM lego WHERE collection = :collection');
+            $stmt->bindParam(':collection', $collection);
+            $stmt->execute();
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $legos = [];
+            foreach ($rows as $row) {
+                $lego = new Lego($row['id'], $row['name'], $row['collection']);
+                $lego->setDescription($row['description']);
+                $lego->setPrice($row['price']);
+                $lego->setPieces($row['pieces']);
+                $lego->setBoxImage($row['imagebox']);
+                $lego->setBackgroundImage($row['imagebg']);
+                $legos[] = $lego;
+            }
+
+            return $legos;
+        }
 }
+?>
 
