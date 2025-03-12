7c34c26b29e8b25ea63ce4b43fbbbe473c1e8220
bastienggg <bastienguitard8@gmail.com>
2025-03-12 16:22:01 +0100

    tp6 exo4 fini

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,27 +5,30 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'lego_store_login')]
-     public function index(AuthenticationUtils $authenticationUtils): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
-         // get the login error if there is one
-         $error = $authenticationUtils->getLastAuthenticationError();
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
 
-         // last username entered by the user
-         $lastUsername = $authenticationUtils->getLastUsername();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
 
-
-       return $this->render('security/login.html.twig', [
+        return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
-           'error'         => $error,
+            'error' => $error,
         ]);
+    }
 
-
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        // controller can be blank: it will never be executed!
+        throw new \Exception('Dont forget to activate logout in security.yaml');
     }
 }
